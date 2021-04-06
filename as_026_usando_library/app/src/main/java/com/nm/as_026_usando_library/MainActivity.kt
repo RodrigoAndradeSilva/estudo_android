10f4ac15915056d192209f387c9486a2184326e6
Rodrigo Andrade Silva <RodrigoAndradeSilva@users.noreply.github.com>
2021-04-05 23:33:50 -0300

    Usando a Library

---
+++ as_026_usando_library/app/src/main/java/com/nm/as_026_usando_library/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.nm.as_026_usando_library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nm.empresa_lib.widgets.DtEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
